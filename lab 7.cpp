0d557cadbb90c5ba793165139e1895d9bd89c04b
lenok-ar <leno4ka279a@gmail.com>
2024-12-04 15:45:18 +0700

    Update lab 7.cpp

--- lab 7.cpp
+++ lab 7.cpp
@@ -5,44 +5,39 @@
 #include <map>
 #include <locale>
 
-// подключаем библиотеку
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "RU");
 
-    ifstream lab7;  // создаём объект класса ifstream
+    ifstream lab7;  
     lab7.open("lab 7.txt");
-    // открываем файл
 
-    map<char, int> bukva; // Частота букв
-    map<string, int> parabykv;   // Частота пар букв
-    char prevCh = '\0'; // Для хранения предыдущего символа
+    map<char, int> bukva; 
+    map<string, int> parabykv;   
+    char prevCh = '\0'; 
     char a;
 
-    while (lab7.get(a)) { // Считываем текст из файла
+    while (lab7.get(a)) { 
         if (isalpha(a)) {
-            a = tolower(a); // Приводим к нижнему регистру
-            bukva[a]++; // Увеличиваем частоту буквы
-
-            // Считаем пару, если предыдущий символ не нулевой
+            a = tolower(a); 
+            bukva[a]++;
+            
             if (prevCh) {
                 parabykv[string(1, prevCh) + a]++;
             }
-            prevCh = a; // Обновляем предыдущий символ
+            prevCh = a;
         }
     }
 
-    lab7.close(); // Закрываем файл
+    lab7.close(); 
 
-    // Вывод частоты букв
     cout << "Частота букв:" << endl;
     for (const auto& entry : bukva) {
         cout << entry.first << ": " << entry.second << endl;
     }
 
-    // Вывод частоты двухбуквенных сочетаний
     cout << "\nЧастота двухбуквенных сочетаний:" << endl;
     for (const auto& entry : parabykv) {
         cout << entry.first << ": " << entry.second << endl;
